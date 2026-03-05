b014b59fcc53073e8b3ba209a265107844f2112e
sbakoren <sbakoren@gmail.com>
2026-03-05 16:51:21 +0100

    Création de la classe Creature

--- combat_rpg.py
+++ combat_rpg.py
@@ -7,5 +7,32 @@ def lancer_des(nb_des, faces):
 
     for i in range(nb_des):
         total += random.randint(1, faces)
-
     return total
+
+
+class Creature:
+
+    def __init__(self, nom, description, pv, defense, typeDegats):
+
+        self.nom = nom
+        self.description = description
+        self.pv = pv
+        self.defense = defense
+        self.typeDegats = typeDegats
+
+        self.etats = []
+        self.actions = []
+        self.initiative = 0
+
+    def est_vivant(self):
+        return self.pv > 0
+
+    def afficher_caracteristiques(self):
+
+        print("\n--- PERSONNAGE ---")
+        print("Nom :", self.nom)
+        print("Description :", self.description)
+        print("PV :", self.pv)
+        print("Défense :", self.defense)
+        print("Type dégâts :", self.typeDegats)
+        print("Etats :", self.etats)
