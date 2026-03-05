923e5b5e3498cb863f6c3061ac8e5caad923a4f9
sbakoren <sbakoren@gmail.com>
2026-03-05 16:47:59 +0100

    Création de la fonction générale de gestion des dés

--- combat_rpg.py
+++ combat_rpg.py
@@ -1,1 +1,11 @@
-print("Bienvenue dans le système de combat RGPD")
+import random
+print("Bienvenue dans le système de combat RPG")
+
+
+def lancer_des(nb_des, faces):
+    total = 0
+
+    for i in range(nb_des):
+        total += random.randint(1, faces)
+
+    return total
